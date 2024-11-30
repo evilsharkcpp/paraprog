173755b45b7bc58219a95879f4f580cc5e557e14
evilsharkcpp <asaulyuk2013@yandex.ru>
2024-12-01 02:05:05 +0700

    Added MPI Tools

--- Main/main.cpp
+++ Main/main.cpp
@@ -1,5 +1,6 @@
 #include <RowSparseMatrix.hpp>
 #include <PlotMatrix.hpp>
+#include <MPITools.hpp>
 
 #include <iostream>
 #include <memory>
@@ -8,9 +9,29 @@
 #include <mpi.h>
 #endif
 
-using namespace Paraprog;
+using namespace Paraprog::MatrixLib;
 
 int main(int argc, char **argv)
 {
+    MPI_Init(&argc, &argv);
+    int rank, size;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    if (size > 0)
+    {
+        RowSparseMatrixPtr left;
+        RowSparseMatrixPtr right;
+        if (rank == 0)
+        {
+            left = std::make_shared<RowSparseMatrix>(RowSparseMatrix::ConvertMatrix({{1, 2, 3}, {1, 2, 3}, {1, 2, 3}}));
+            right = std::make_shared<RowSparseMatrix>(RowSparseMatrix::ConvertMatrix({{1, 2, 3}, {1, 2, 3}, {1, 2, 3}}));
+        }
+        auto res = MPITools::MultRowSparseMatrices(left, right);
+        MPI_Finalize();
+        if (res != nullptr)
+        {
+            std::cout << res->GetPrettyMatrix();
+        }
+    }
     return 0;
 }
